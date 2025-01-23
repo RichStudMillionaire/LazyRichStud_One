bd2cd95fabf79d5cb7ac4a52969c1ae447404bec
RichStudMillionaire <lazyrichstud@gmail.com>
2025-01-23 12:02:24 -0500

    On branch main Initial commit new file:   .gitignore new file:   functions.py new file:   interface.py new file:   main.py new file:   nasdaq.csv new file:   nyse.csv new file:   objects.py

---
+++ objects.py
@@ -0,0 +1,9 @@
+# LAZY RICH STUD VERSION 1.0
+# OBJECTS AND CLASSES
+class TickerExtractor:
+    def __init__(self, csv_file):
+        self.csv = csv_file
+        
+    def open_csv():
+        try 
+        
