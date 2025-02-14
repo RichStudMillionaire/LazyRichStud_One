6763e95963b632cecc08229b000d7ff297c6fe30
RichStudMillionaire <lazyrichstud@gmail.com>
2025-02-14 11:40:30 -0500

    3 systems : growth, dividend, customizable

--- systems.py
+++ systems.py
@@ -4,8 +4,11 @@ import yfinance as yf
 import json
 import os
 from datetime import datetime
+from datetime import date
+from dateutil.relativedelta import relativedelta
 import csv
 import time
+import operator
 
 class TickerInfo:
     def __init__(self):
@@ -136,6 +139,21 @@ class TickerInfo:
             print("{} : {}".format(key,value))
         print()
         
+    def sort_ascending(self, dict_list, key):
+        try:
+            sorted_list = sorted(dict_list, key=operator.itemgetter(key))
+        except Exception as e:
+            print("Could not sort list in ascending order with {} as key : {}".format(key,e))
+        return sorted_list
+    
+    def sort_descending(self, dict_list, key):
+        try:
+            sorted_list = sorted(dict_list, key=operator.itemgetter(key), reverse=True)
+        except Exception as e:
+            print("Could not sort list in descending order with {} as key : {}".format(key,e))
+        return sorted_list
+        
+        
 
 class HardDrive:
     def __init__(self):
@@ -173,6 +191,8 @@ class Timestamp:
         pass
     def now(self):
         return datetime.now()
+    def today(self):
+        return date.today()
     
 class ChunkManager:
     def __init__(self, chunk_size, pause_time_in_seconds, ticker_list):
@@ -232,9 +252,66 @@ class ExtractTickersFromCsv:
         print("Expected : {}".format(len(self.nasdaq)+len(self.nyse)))
         print(self.nasdaq_and_nyse)
         
+class Salary:
+    def __init__(self):
+        self.salary = 1
+        self.pay_frequency = 'Biweekly'
+        self.raise_frequency = 'Monthly'
+        self.raise_rate = .25
+    def renegociate(self, new_salary):
+        self.salary = new_salary
+    def set_first_day_of_work(self):
+        pass
+    def print_compensation_report_current(self):
+        pass
+    def print_compensation_report_history(self):
+        pass
+
+class Ledger:
+    def __init__(self):
+        self.hard_drive = HardDrive()
+        self.gross_revenue = 0
+        self.net_revenue = 0
+        self.salary_paid = 0
+        self.ledger = []
+    
+    def buy_stock(self, ticker, qty, price):
+        ledger_entry = {}
+        ledger_entry['symbol'] = ticker
+        ledger_entry['transaction'] = 'Buy'
+        ledger_entry['qty'] = qty
+        ledger_entry['UnitaryBuyingPrice'] = price
+        total_cost = qty*price
+        ledger_entry['totalCost'] = total_cost
+        self.post(ledger_entry,0)
+    
+    def sell_stock(self, ticker, qty, price):
+        ledger_entry = {}
+        ledger_entry['symbol'] = ticker
+        ledger_entry['transaction'] = 'Sell'
+        ledger_entry['qty'] = qty
+        ledger_entry['UnitaryBuyingPrice'] = price
+        total_cost = qty*price
+        ledger_entry['totalCost'] = total_cost
+        self.post(ledger_entry,1)
         
+    def post(self, ledger_entry, transaction_int):
+        self.transaction = []
+        self.buy = {}
+        self.sell = {}
+    
+        if transaction_int == 0:
+            self.buy = ledger_entry
+        elif transaction_int == 1:
+            self.sell = ledger_entry
+    
+    def read(self):
+        pass
+        
+
 class GrowthSystem:
     def __init__(self):
+        self.id = "Growth System"
         self.hard_drive = HardDrive()
         self.info = TickerInfo()
         self.nasdaq = []
@@ -276,6 +353,7 @@ class GrowthSystem:
         
 class DividendSystem:
     def __init__(self):
+        self.id = "Dividends System"
         self.hard_drive = HardDrive()
         self.info = TickerInfo()
         self.nasdaq = []
@@ -307,6 +385,79 @@ class DividendSystem:
         self.filtered_list = self.info.payout_ratio_filter(self.filtered_list)
         self.finalists = self.info.get_str_from_dict(self.filtered_list)
         print(self.finalists)
+        
+class CustomizableSystem:
+    def __init__(self):
+        self.id = "Customizable System"
+        self.complete_list = []
+        self.filtered_list = []
+        self.hard_drive = HardDrive()
+        self.info = TickerInfo()
+        self.nasdaq = []
+        self.nyse = []
+        self.nasdaq_and_nyse = []
+        self.booth()
+    def booth(self):
+        self.check_for_active_session()
+    def check_for_active_session(self):
+        self.complete_list=self.hard_drive.load("CustomizableSystem.json")
+        if self.complete_list == False:
+            print("No active session : Starting a new session, please wait")
+            self.start_new_session()
+        else:
+            print("Restoring active session ... Please wait")
+            self.restore_session()
+    def start_new_session(self):
+        tickers = ExtractTickersFromCsv("nasdaq.csv","nyse.csv")
+        self.nasdaq = tickers.get_nasdaq()
+        self.nyse = tickers.get_nyse()
+        self.nasdaq_and_nyse = tickers.get_nasdaq_plus_nyse()
+        self.collect_ticker_info(self.nasdaq_and_nyse)
+    def collect_ticker_info(self, ticker_list):
+        info_list=self.info.get_info_from_ticker_list(ticker_list)
+        self.hard_drive.save(info_list, "CustomizableSystem.json")
+    def restore_session(self):
+        self.complete_list = self.hard_drive.load('CustomizableSystem.json')
+        self.filtered_list = self.complete_list
+    def filter_50_day_moving_average(self, gap):
+        dict_list = self.filtered_list
+        self.filtered_list=self.info.below_fifty_day_moving_average_filter(dict_list, gap)
+        return self.filtered_list
+    def filter_52_week_low(self, gap):
+        dict_list = self.filtered_list
+        self.filtered_list=self.info.fifty_two_week_low_filter(dict_list, gap)
+        return self.filtered_list
+    def filter_below_price_target(self, gap):
+        dict_list = self.filtered_list
+        self.filtered_list = self.info.below_median_price_target_filter(dict_list, gap)
+        return self.filtered_list
+    def filter_min_stock_price(self, min_price):
+        dict_list = self.filtered_list
+        self.filtered_list = self.info.no_penny_stocks_filter(dict_list, min_price)
+        return self.filtered_list
+    def filter_payout_ratio(self):
+        dict_list = self.filtered_list
+        self.filtered_list = self.info.payout_ratio_filter(dict_list)
+        return self.filtered_list
+    def filter_low_or_no_dividends(self):
+        dict_list = self.filtered_list
+        self.filtered_list = self.info.no_or_low_dividend_filter(dict_list)
+        return self.filtered_list
+    def filter_high_dividends(self):
+        dict_list = self.filtered_list
+        self.filtered_list = self.info.high_dividend_filter(dict_list)
+        return self.filtered_list
+        
+    def print_finalists_rpt(self):
+        print("{} results matching your criteria".format(len(self.filtered_list)))
+        print()
+        for slim_dict in self.filtered_list:
+            self.info.print_single_ticker_rpt(slim_dict)
+    def clear_filters(self):
+        self.filtered_list = self.complete_list
+    
+    
+        
         
         
         
